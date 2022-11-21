2d2550901277934af426e0a4faaa5ec54c4f1c25
Jimodo Joretta Ogada <84725378+Jimodo@users.noreply.github.com>
2022-11-20 18:01:16 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css'
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import { ThemeProvider } from 'next-themes'
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
